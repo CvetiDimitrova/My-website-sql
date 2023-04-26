98365fb4a35e8c598e3409f1e9e6691f664388d1
Cvetozara Dimitrova <cvetozara.dimitrova14@gmail.com>
2023-04-26 12:20:59 +0300

    Login,Register

--- src/java/LoginServlet.java
+++ src/java/LoginServlet.java
@@ -5,20 +5,42 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @WebServlet(urlPatterns = {"/LoginServlet"})
 public class LoginServlet extends HttpServlet {
-
    
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        String user = request.getParameter("user");
+        String name = request.getParameter("name");
         String pass = request.getParameter("pass");
-        if (user.equals("cveti") && pass.equals("1234")) {
-            response.getWriter().println("Login successful");
-        }else{
-             response.getWriter().println("Login failed");
+        
+        try {
+            Class.forName("org.mariadb.jdbc.Driver");
+            Connection conn = DriverManager.getConnection("jdbc:mariadb://localhost/mmc");
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(String.format("SELECT COUNT(*) FROM potrebiteli WHERE name=\"%s\"", name));
+            rs.next();
+            ResultSet rs1 = stmt.executeQuery(String.format("SELECT pass FROM potrebiteli WHERE name=\"%s\"", name));
+            rs1.next();
+            
+            if(rs.getString(1).equals("1")) {
+                if(rs1.getString(1).equals(pass)) {
+                    response.getWriter().println("Login successful");
+                } else response.getWriter().println("Login failed");
+            } else response.getWriter().println("Login failed");
+            
+            
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(LoginServlet.class.getName()).log(Level.SEVERE, null, ex);
         }
+        
     }
 }
--- src/java/RegisterServlet.java
+++ src/java/RegisterServlet.java
@@ -1,5 +1,5 @@
+
 import java.io.IOException;
-import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -15,33 +15,31 @@ import java.util.logging.Logger;
 
 @WebServlet(urlPatterns = {"/RegisterServlet"})
 public class RegisterServlet extends HttpServlet {
-
    
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        String user = request.getParameter("user");
+        String name= request.getParameter("name");
         String pass = request.getParameter("pass");
+        String email = request.getParameter("mail");
         
         try {
-            Class.forName("org.mariadb.jbc.Driver");
-            Connection conn = DriverManager.getConnection("jbc:mariadb://localhost/traktori");
+            Class.forName("org.mariadb.jdbc.Driver");
+            Connection conn = DriverManager.getConnection("jdbc:mariadb://localhost/mmc");
             Statement stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT * FROM marki");
+            ResultSet rs1 = stmt.executeQuery(String.format("SELECT COUNT(*) FROM potrebiteli WHERE name=\"%s\"", name));
+            rs1.next();
+            if(rs1.getString(1).equals("1")) {
+                response.getWriter().println("User exists, registration failed");
+            } else {
+                String q = String.format("INSERT INTO potrebiteli VALUES(\"%s\", \"%s\", \"%s\")", name, pass, email);
+                ResultSet rs = stmt.executeQuery(q);
+                response.getWriter().println("Registration successful");
+            }
             conn.close();
             
-            while(rs.next()) {
-                int id = rs.getInt(1);
-                String marki = null;
-                response.getWriter().println(id + " " + marki);
-            }
-        } catch (ClassNotFoundException ex) {
-            Logger.getLogger(RegisterServlet.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (SQLException ex) {
+        } catch (ClassNotFoundException | SQLException ex) {
             Logger.getLogger(RegisterServlet.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
-        response.getWriter().println("Register successful");
-       
     }
 }
